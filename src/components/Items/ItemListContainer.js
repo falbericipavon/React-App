935e2e44a46b76fde1400fc4b0b2426c8eb2e045
Fernanda Alberici <56327315+falbericipavon@users.noreply.github.com>
2022-06-07 14:52:08 -0300

    Desafío 4: Contador con botón

--- src/components/Items/ItemListContainer.js
+++ src/components/Items/ItemListContainer.js
@@ -1,13 +1,14 @@
-import React, { useEffect, useState } from 'react';
+import ItemCount from "./ItemCount";
 
-
-const ItemListContainer = () =>{
-  
+const ItemListContainer = ({onAdd}) =>{
     return (
        <>
-         
+         <h1>Lista de items</h1> 
+         <div className="container mt-5">
+            <p>Camisa</p>
+            <ItemCount stock="5" onAdd={onAdd}/>
+         </div>
        </>
       );
 }
-
 export default ItemListContainer
